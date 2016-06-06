61b64f74b3e0254f1dee16bd1ec5380f373f5daa
Maheswari014 <mahethangaraj1996@gmail.com>
2016-06-06 11:51:42 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int n;
+scanf("%d",&n);
+if (n==0)
+{
+ printf("the num is zero");
+if(n>0)
+{
+   printf("the num is pos");
+ if (n<0)
+ {
+   printf("the num is neg");
+ }
+}
+}
+getch();
